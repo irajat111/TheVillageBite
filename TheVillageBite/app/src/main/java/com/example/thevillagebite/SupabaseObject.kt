21712aadd8411ae87222e46aa678f41e96eceecc
Rajat Singh <140708636+irajat111@users.noreply.github.com>
2026-07-16 17:04:06 +0530

    Complete Project
    Push all

--- TheVillageBite/app/src/main/java/com/example/thevillagebite/SupabaseObject.kt
+++
@@ -1,6 +0,0 @@
-package com.example.thevillagebite
-
-object SupabaseObject {
-    const val supaBaseUrl = "https://bpiqwtvcpwqcmqiyynuu.supabase.co"
-    const val supaBasekey = "sb_publishable_D6OsW6Fs88RTSzAFgIQh0A_CBY_2uv1"
-}
